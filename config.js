7387fcec1eed6217440dd39dceeef7f542999b25
georgey <yegh98@yahoo.com>
2011-08-29 23:21:09 -0700

    server code check in

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {};
+
+config.maxFiles = 1;
+config.filePrefix = "confFile";
+config.fileSuffix = ".conf";
+config.fileLocation = "./";
+config.logFile = './trace.log';
+config.logLevel = 3;
+
+module.exports = config;
