63a963893e8d634743d5842d455da1c2df7d9296
TJ Krusinski <tjkrus@gmail.com>
2014-05-23 14:31:41 -0400

    fix up tests to work

--- test/test.large.js
+++ test/test.large.js
@@ -5,11 +5,9 @@
 var autocomplete = require('../lib/autocomplete'),
     sys = require('sys'),
     fs = require('fs');
+    assert = require('assert');
 
 require('./common');
-
-process.cwd().should.include.string('autocomplete');
-
 
 // Read the really big data file into memory
 function readLines(input, onReady) {
@@ -39,7 +37,8 @@ readLines(input, function(words) {
         var duration = new Date() - start;
         console.log('Elapsed time to search dictionary for 3 character prefix: ' + duration + 'ms');
 
-        matches.length.should.eql(663);
+        var keys = Object.keys(matches);
+        assert.equal(keys.length, 663);
     });
 
     a.initialize(function(onReady) {
